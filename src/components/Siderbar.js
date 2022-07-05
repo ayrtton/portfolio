49789ad0bff98556bd93b2a62698d9daf6c0c354
Ayrton <ayrtton.vinicius@gmail.com>
2022-07-04 21:18:15 -0300

    Enable visibility useState for other items

--- src/components/Siderbar.js
+++ src/components/Siderbar.js
@@ -4,31 +4,31 @@ import { FaUser, FaSuitcase, FaTools, FaMailBulk } from "react-icons/fa"
 import { useState } from "react"
 
 function Sidebar() {
-    const [isVisible, setIsVisible] = useState(false)
+    const [isVisible, setIsVisible] = useState(0)
 
     return (
         <div className={styles.sidebar}>
             <ul>
-                <li onMouseLeave={() => setIsVisible(false)}>
-                    {isVisible && <div className={styles.iconTitle}>Home</div>}
-                    <IoHome className={styles.icon} onMouseOver={() => setIsVisible(true)} />
+                <li onMouseLeave={() => setIsVisible(0)}>
+                    {isVisible === 1 && <div className={styles.iconTitle}>Home</div>}
+                    <IoHome className={styles.icon} onMouseOver={() => setIsVisible(1)} />
                 </li>
-                {/*<li>
-                    <div className={styles.iconTitle}>About</div>
-                    <FaUser className={styles.icon} />
+                <li onMouseLeave={() => setIsVisible(0)}>
+                    {isVisible === 2 && <div className={styles.iconTitle}>About</div>}
+                    <FaUser className={styles.icon} onMouseOver={() => setIsVisible(2)} />
                 </li>
-                <li>
-                    <div className={styles.iconTitle}>Experience</div>
-                    <FaSuitcase className={styles.icon} />
+                <li onMouseLeave={() => setIsVisible(0)}>
+                    {isVisible === 3 && <div className={styles.iconTitle}>Experience</div>}
+                    <FaSuitcase className={styles.icon} onMouseOver={() => setIsVisible(3)}/>
                 </li>
-                <li>
-                    <div className={styles.iconTitle}>Personal Projects</div>
-                    <FaTools className={styles.icon} />
+                <li onMouseLeave={() => setIsVisible(0)}>
+                    {isVisible === 4 && <div className={styles.iconTitle}>Personal Projects</div>}
+                    <FaTools className={styles.icon} onMouseOver={() => setIsVisible(4)}/>
                 </li>
-                <li>
-                    <div className={styles.iconTitle}>Contact</div>
-                    <FaMailBulk className={styles.icon} />
-                </li>*/}
+                <li onMouseLeave={() => setIsVisible(0)}>
+                    {isVisible === 5 && <div className={styles.iconTitle}>Contact</div>}
+                    <FaMailBulk className={styles.icon} onMouseOver={() => setIsVisible(5)}/>
+                </li>
             </ul>
         </div>
     )
