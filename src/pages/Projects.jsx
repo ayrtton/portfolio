d9b5dd8d23bee72681ecb0deb9e80d06eca7fc8e
Ayrton <ayrtton.vinicius@gmail.com>
2022-08-26 15:47:30 -0300

    Create content for projects page

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -1,10 +1,17 @@
 import styles from "./Projects.module.css"
+import image from "./../images/loginSignUp.JPG"
 
 function Projects() {
     return (
         <div className={styles.projectsContent}>
             <h1>Projects</h1>
             <span id={styles.constructionMessage}>Under construction...</span>
+            <div className={styles.projectList}>
+                <div className={styles.project}>
+                    <img className={styles.image} src={ image } alt="" />
+                    <a href="https://github.com/ayrtton/login-signup">Login and Sign Up System</a>
+                </div>
+            </div>
         </div>
     )
 }
