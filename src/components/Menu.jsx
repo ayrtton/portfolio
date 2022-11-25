faab8149194355c26761e0f4da48643aeaa309b5
Ayrton <ayrtton.vinicius@gmail.com>
2022-11-25 10:30:46 -0300

    Create CSS fade effects for the side menu titles

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -2,31 +2,28 @@ import styles from "./Menu.module.css"
 import { IoHome } from "react-icons/io5"
 import { FaUserTie } from "react-icons/fa"
 import { FaCode} from "react-icons/fa"
-import { useState } from "react"
 import { Link } from "react-router-dom"
 
 function Menu() {
-    const [isVisible, setIsVisible] = useState(0)
-
     return (
         <div className={styles.menuContent}>
             <ul>
-                <li onMouseLeave={() => setIsVisible(0)}>
+                <li>
                     <Link to="/">
-                        {isVisible === 1 && <div className={styles.iconTitle}>Home</div>}
-                        <IoHome className={styles.icon} onMouseOver={() => setIsVisible(1)} />
+                        <div className={styles.iconTitle}>Home</div>
+                        <IoHome className={styles.icon}/>
                     </Link>
                 </li>
-                <li onMouseLeave={() => setIsVisible(0)}>
+                <li>
                     <Link to="/about">
-                        {isVisible === 2 && <div className={styles.iconTitle}>About Me</div>}
-                        <FaUserTie className={styles.icon} onMouseOver={() => setIsVisible(2)} />
+                        <div className={styles.iconTitle}>About Me</div>
+                        <FaUserTie className={styles.icon}/>
                     </Link>
                 </li>
-                <li onMouseLeave={() => setIsVisible(0)}>
+                <li>
                     <Link to="/projects">
-                        {isVisible === 3 && <div className={styles.iconTitle}>Projects</div>}
-                        <FaCode className={styles.icon} onMouseOver={() => setIsVisible(3)} />
+                        <div className={styles.iconTitle}>Projects</div>
+                        <FaCode className={styles.icon}/>
                     </Link>
                 </li>
             </ul>
