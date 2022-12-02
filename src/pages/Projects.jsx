9b8f386e92c6f83b58fcbb565752c277ea56abc1
Ayrton <ayrtton.vinicius@gmail.com>
2022-12-02 11:06:53 -0300

    Set projects responsiveness for desktop devices

--- src/pages/Projects.jsx
+++ src/pages/Projects.jsx
@@ -5,20 +5,20 @@ import project02 from "./../images/project02.jpg"
 function Projects() {
     return (
         <div className={styles.main}>
-            <div className="projectsSection">
-                <h1>Projects</h1>
-                <span id={styles.constructionMessage}>Under construction...</span>
-                <div className={styles.projectList}>
+            <h1>Projects</h1>
+            <span id={styles.constructionMessage}>Under construction...</span>
+            <div className={styles.projectList}>
+                <div className={styles.row}>
                     <div className={styles.project}>
-                        <img className={styles.image} src={ project01 } alt="" />
-                        <a href="https://github.com/ayrtton/login-signup" target="_blank" rel="noreferrer">Authentication System [Laravel and React]</a>
+                        <div id={styles.image1}></div>
+                        <a className={styles.projectTitle} href="https://github.com/ayrtton/login-signup" target="_blank" rel="noreferrer">Authentication System [Laravel and React]</a>
                     </div>
                     <div className={styles.project}>
-                        <img className={styles.image} src={ project02 } alt="" />
-                        <a href="https://github.com/ayrtton/auth-system-codeigniter" target="_blank" rel="noreferrer">Authentication System [CodeIgniter]</a>
+                        <div id={styles.image2}></div>
+                        <a className={styles.projectTitle} href="https://github.com/ayrtton/auth-system-codeigniter" target="_blank" rel="noreferrer">Authentication System [CodeIgniter]</a>
                     </div>
                 </div>
-            </div>
+            </div>  
         </div>
     )
 }
